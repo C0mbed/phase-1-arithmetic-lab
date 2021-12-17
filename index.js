13d1026ecc475f0e0b81a251257a7b434fed33d0
C0mbed <matt@farncombe.co>
2021-12-17 07:01:11 -0700

    commit tree 1f3772f69dcf86ff04be3fc63384fbaa9cf066c8

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+let max = 20;
